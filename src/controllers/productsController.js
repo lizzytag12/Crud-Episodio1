672fb6d7dca7492e18021039681f88cb430bfea4
Luisa <luisa.h.morales@gmail.com>
2022-05-12 00:02:07 -0300

    metodo guardar producto terminado

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,31 +1,53 @@
 const fs = require('fs');
 const path = require('path');
-
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
+
+const leerProductos=()=>{
+
+	
+    const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8')); 
+	return products
+}
+
+/* funcion para guardar productos */
+const salvarProductos=(products)=>fs.writeFileSync(productsFilePath, JSON.stringify(products,null,3))
+
 
 /**
  * It takes a number, converts it to a string, adds commas to it, and returns the result
  */
 const toThousand = n => n.toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
+
+
+
 const controller = {
+
+
+
 	// Root - Show all products
 	/* A function that is being exported to the controller.js file. */
 	/* renderizo a la vista productos los productos que tengo y mando el metodo toThousand */
 	index: (req, res) => {
-	 return res.render('products',{
+
+		let products = leerProductos()
+
+	    return res.render('products',{
 		 products,
 		 toThousand
 
-	 })
+	    })
 	},
+
+
 
 	// Detail - Detail from one product
 	/* A function that is being exported to the controller.js file. It is rendering the view detail and
 	sending the product and the method toThousand. */
 	detail: (req, res) => {
-		const product = products.find(product=> product.id === +req.params.id)
+		const product = leerProductos().find(product=> product.id === +req.params.id)
 		return res.render('detail',{
 
 			product,
@@ -33,14 +55,49 @@ const controller = {
 		})
 	},
 
+
+
 	// Create - Form to create
 	create: (req, res) => {
-		// Do the magic
+		/* Rendering the view product-create-form. */
+		return res.render ('product-create-form')
 	},
+	
+	
 	
 	// Create -  Method to store
 	store: (req, res) => {
-		// Do the magic
+
+	    /* Reading the products from the database. */
+		let products = leerProductos()
+
+		/* Destructuring the req.body object. */
+		const {name,price,discount,description,category} = req.body;
+
+	    /* Creating a new product with the data that the user is sending. */
+		let nuevoProducto={
+
+			id : products[products.length - 1].id +1,
+			name : name.trim(),
+			description : description.trim(),
+			price: +price,
+			discount : +discount,
+			image : "default-image.png",
+			category
+
+
+
+		}
+     /* Adding the new product to the array of products. */
+		products.push(nuevoProducto)
+
+		/* Saving the products in the database. */
+		salvarProductos(products)
+
+
+
+		/* Redirecting the user to the products page. */
+		return res.redirect('/products');
 	},
 
 	// Update - Form to edit
