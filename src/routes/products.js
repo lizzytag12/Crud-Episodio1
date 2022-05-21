c4f1b16d6bb6545d5eb63e58017de454963d379a
Luisa <luisa.h.morales@gmail.com>
2022-05-21 01:34:18 -0300

    upload agregado a la ruta

--- src/routes/products.js
+++ src/routes/products.js
@@ -34,7 +34,7 @@ router.get('/detail/:id', productsController.detail);
 
 /*** EDIT ONE PRODUCT ***/ 
 router.get('/edit/:id', productsController.edit); 
-router.put('/update/:id', productsController.update); 
+router.put('/update/:id',upload.single('image'), productsController.update); 
 
 
 /*** DELETE ONE PRODUCT***/ 
